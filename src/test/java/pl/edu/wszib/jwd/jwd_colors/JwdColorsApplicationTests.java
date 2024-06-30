3eb735b8d04604677eac835b889423e7aad9f999
gadamski <gadamski@student.wszib.edu.pl>
2024-06-30 09:16:15 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/jwd/jwd_colors/JwdColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.jwd_colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdColorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
